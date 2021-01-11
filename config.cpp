d214f53686d2a7237447d3448b74ce8a7adb8f11
Peter Braun <pbraun@redhat.com>
2021-01-12 00:26:04 +0100

    change text colors

--- config.cpp
+++ config.cpp
@@ -5,5 +5,4 @@ const char *configWindowTitle = "Zwei";
 const int configWindowWidth = St::instance().getWindowSize().w;
 const int configWindowHeight = St::instance().getWindowSize().h;
 const unsigned int configTileSize = 16;
-const float configTargetFramerate = St::instance().getFps();
 const float configZoomFactor = 4.0f;
